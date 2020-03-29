f6fa4b4329f908f82434d6e73edcc3f0e4a7d572
Marc Sauter <marc.sauter@bluewin.ch>
2020-03-29 21:49:58 +0200

    :construction:

--- manufacturer.go
+++ manufacturer.go
@@ -5,6 +5,7 @@ import "encoding/xml"
 // Manufacturer https://www.streit.cc/extern/uddf_v321/en/manufacturer.html
 type Manufacturer struct {
 	XMLName   xml.Name `xml:"manufacturer"`
+	ID        string   `xml:"id,attr"`
 	Name      string   `xml:"name"`
 	Aliasname string   `xml:"aliasname,omitempty"`
 	Address   Address  `xml:"address,omitempty"`
